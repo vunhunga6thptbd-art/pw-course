e8d3b670495fb2136fe6634ffd6c3a3d5f1ed002
Hong Nhung <vunhunga6thptbd@gmail.com>
2026-03-04 17:32:12 +0700

    add bai tap

--- tests/lesson-02/ex1.js
+++ tests/lesson-02/ex1.js
@@ -1,13 +1,13 @@
 //a. Khai báo một hằng số number với giá trị là 12.
-const number = 12;
+    const number = 12;
 
 //b. Khai báo một biến name với giá trị là ”my number”
-let name = "my number";
+    let name = "my number";
 
 //c. Khai báo một biến isEven với giá trị là sai.
-let isEven = false;
+    let isEven = false;
 
 //d. In ra kiểu dữ liệu của các biến đã khai báo, sử dụng typeof
-console.log(typeof number);
-console.log(typeof name);
-console.log(typeof isEven);
+    console.log(typeof number);
+    console.log(typeof name);
+    console.log(typeof isEven);
--- tests/lesson-02/ex3.js
+++ tests/lesson-02/ex3.js
@@ -1,25 +1,29 @@
 /*
-Tạo file ex3.js, thêm vào đáp án cho câu hỏi sau: Cho 2 biến: a = true, b = false, c =
-true. Theo bạn, kết quả của các biểu thức sau là gì?
-a. a && b && c
-b. a && b || c
+Tạo file ex3.js, thêm vào đáp án cho câu hỏi sau: Cho 2 biến: a = true, b = false, c = true. 
+Theo bạn, kết quả của các biểu thức sau là gì?
+    a. a && b && c
+    b. a && b || c
 
 Trả lời:
 a. a && b && c
     true && false && true
-=> Kết quả: false
+=> Kết quả: 
     Do && trả về đúng nếu cả 2 vế của mệnh đề đúng
+        a && b = false
+        b && c = false
+            => a && b && c = false
 
 b. a && b || c
     true && false || true
-=> Kết quả: true
-    Do a && b = false
-       b || c = true
-        => a && b || c = true
+=> Kết quả:
+    Do || trả về đúng nếu một trong 2 vế của mệnh đề đúng
+        a && b = false
+        b || c = true
+            => a && b || c = true
 */
-let a = true
-let b = false
-let c = true
+    let a = true
+    let b = false
+    let c = true
 
-console.log(a && b &&c );
-console.log(a && b || c);
+    console.log(a && b &&c );
+    console.log(a && b || c);
