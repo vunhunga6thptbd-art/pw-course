6b2393adb0c2c9a517726eab3269fa121a42d9e8
Hong Nhung <vunhunga6thptbd@gmail.com>
2026-03-07 11:56:23 +0700

    fix: bai tap lesson-03

--- tests/lesson-03/ex3.js
+++ tests/lesson-03/ex3.js
@@ -1,9 +1,9 @@
 /*
-3. Tạo file ex3.js, thêm vào code đáp án cho đề bài sau:
-    a. Lặp từ 1 tới 100
-    b. Trong mỗi vòng lặp, in ra:
-        i. Nếu giá trị của vòng lặp chia hết cho 2, in “Số <i> là số chẵn”
-        ii. Nếu giá trị của vòng lặp không chia hết cho 2, in “Số <i> là số lẻ”
+    3. Tạo file ex3.js, thêm vào code đáp án cho đề bài sau:
+        a. Lặp từ 1 tới 100
+        b. Trong mỗi vòng lặp, in ra:
+            i. Nếu giá trị của vòng lặp chia hết cho 2, in “Số <i> là số chẵn”
+            ii. Nếu giá trị của vòng lặp không chia hết cho 2, in “Số <i> là số lẻ”
 */
 
 for (let i = 1; i <= 100; i++) {
